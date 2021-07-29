831b35b239b64f6035534f1cad78f9a7f6d41ed6
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-07-29 20:38:37 +0800

    Update sound.py

--- src/sound.py
+++ src/sound.py
@@ -1,10 +1,11 @@
 import sounddevice as sd
 import numpy as np
-# import pyaudio
+import pyaudio
 import soundfile as sf
 from scipy.io.wavfile import write
 import librosa
 # from settings import DATA_DIR, saveWavFile, readWavFile
+from settings import MAX_INPUT_CHANNELS, DEFAULT_SAMPLE_RATE, CHUNK_SIZE, INPUT_DEVICE
 
 duration = 32  # seconds
 fs = 44100 #int for write
@@ -13,13 +14,31 @@ sd.default.channels = 1
 
 class Sound():
     def __init__(self):
+        self.audio = pyaudio.PyAudio()
+        self.format = pyaudio.paInt16
+        self.channels = MAX_INPUT_CHANNELS
+        self.sample_rate = DEFAULT_SAMPLE_RATE
+        self.chunk = CHUNK_SIZE
+        self.device = INPUT_DEVICE
+        
         self.myrecording = np.array([])
         self.fs = 44100
         print("hello")
         
     def recording(self,fn):
-        self.myrecording = sd.rec(int(duration * fs), dtype='int')
-        sd.wait(duration)
+        #Recording using pyaudio
+        self.audio = pyaudio.PyAudio()
+        stream = self.audio.open(
+                        format=self.format,
+                        channels=self.channels,
+                        rate=self.sample_rate,
+                        input=True,
+                        frames_per_buffer=self.chunk,
+                        input_device_index=self.device)
+        
+        #Recording using sounddevice
+        # self.myrecording = sd.rec(int(duration * fs), dtype='int')
+        # sd.wait(duration)
         
         # Save as WAV file # **fs needs int dtype
         # filename = saveWavFile(fn)
