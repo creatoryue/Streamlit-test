987fd6bb2c7bb217790056baa6b8ec99db35becb
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-07-29 13:45:41 +0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,23 @@
+import os
+
+# The Root Directory of the project
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+
+
+MODEL_DIR = os.path.join(ROOT_DIR, 'models')
+MODEL_H5 = os.path.join(MODEL_DIR, 'model.h5')
+
+WAVE_OUTPUT_FILE = os.path.join(DATA_DIR, "recorded.wav")
+
+
+
+
+# Audio configurations
+INPUT_DEVICE = 0
+MAX_INPUT_CHANNELS = 1  # Max input channels
+DEFAULT_SAMPLE_RATE = 44100   # Default sample rate of microphone or recording device
+DURATION = 3   # 3 seconds
+CHUNK_SIZE = 1024
