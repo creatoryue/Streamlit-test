482faea91b928a9a73cc2b0df9eac4c1484265e0
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-07-29 21:45:04 +0800

    Update sound.py

--- src/sound.py
+++ src/sound.py
@@ -47,15 +47,11 @@ class Sound():
         
         
         #Recording using sounddevice
-        self.myrecording = sd.rec(int(DURATION * DEFAULT_SAMPLE_RATE), dtype='int', channels=2)
+        # self.myrecording = sd.rec(int(DURATION * DEFAULT_SAMPLE_RATE), dtype='int', channels=2)
+        # sd.wait(DURATION)
         
-
-        sd.wait(DURATION)
-        
-        # Save as WAV file # **fs needs int dtype
-        # filename = saveWavFile(fn)
-        
-        write(fn, fs, self.myrecording)  
+        # Save as WAV file # **fs needs int dtype        
+        #write(fn, fs, self.myrecording)  
         
         return self.myrecording
     
