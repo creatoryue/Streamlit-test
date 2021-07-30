d633706f301065e16b3898572c473fea9b74c347
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-07-30 17:21:49 +0800

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -39,7 +39,8 @@ stt_button.js_on_event("button_click", CustomJS(code="""
 
 result = streamlit_bokeh_events(
     stt_button,
-    events="GET_TEXT")
+    events="GET_TEXT",
+    key="listen")
 
 #state_recordButton = st.button("Click to Record")
 #if state_recordButton:
