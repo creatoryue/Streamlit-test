f204d01a07ec76cdc43b81cd1409824ff52451da
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-07-30 16:36:11 +0800

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -33,35 +33,11 @@ filename_user = st.text_input('Enter a filename: ')
 
 stt_button = Button(label="Speak", width=100)
 stt_button.js_on_event("button_click", CustomJS(code="""
-    var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
- 
-    recognition.onresult = function (e) {
-        var value = "";
-        for (var i = e.resultIndex; i < e.results.length; ++i) {
-            if (e.results[i].isFinal) {
-                value += e.results[i][0].transcript;
-            }
-        }
-        if ( value != "") {
-            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-        }
-    }
-    recognition.start();
+    <input type="file" accept="audio/*" capture>
+    
     """))
 
-result = streamlit_bokeh_events(
-    stt_button,
-    events="GET_TEXT",
-    key="listen",
-    refresh_on_update=False,
-    override_height=75,
-    debounce_time=0)
 
-if result:
-    if "GET_TEXT" in result:
-        st.write(result.get("GET_TEXT"))
 #state_recordButton = st.button("Click to Record")
 #if state_recordButton:
 #    if filename_user == "":
