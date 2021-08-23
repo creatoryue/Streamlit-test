3f6816d753c3ab16e7acc22af4346dda2255f098
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-08-23 15:10:22 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ DATA_DIR = os.path.join(ROOT_DIR, 'data')
 
 
 MODEL_DIR = os.path.join(ROOT_DIR, 'models')
-MODEL_H5 = os.path.join(MODEL_DIR, 'model.h5')
+MODEL_H5 = os.path.join(MODEL_DIR, 'CNN_for4lungcondition_20210717.h5')
 
 def saveWavFile(fn):    
     WAVE_OUTPUT_FILE = os.path.join(DATA_DIR, "{}.wav".format(fn))
